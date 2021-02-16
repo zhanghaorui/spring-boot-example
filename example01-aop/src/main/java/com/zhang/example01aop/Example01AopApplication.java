120b4f45ef3c89c55a60493736a1b91d84757b51
张浩锐 <zhanghaorui@jczh56.com>
2021-02-16 11:20:49 +0800

    aop-init

---
+++ example01-aop/src/main/java/com/zhang/example01aop/Example01AopApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.example01aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example01AopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example01AopApplication.class, args);
+    }
+
+}
---
+++ example01-aop/src/test/java/com/zhang/example01aop/Example01AopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhang.example01aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example01AopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
