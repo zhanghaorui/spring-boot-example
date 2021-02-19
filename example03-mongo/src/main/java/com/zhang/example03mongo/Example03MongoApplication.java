f1511780064d23b7efd383ef6fc4ad69a2ee6bf5
张浩锐 <Zhr199707>
2021-02-19 10:08:30 +0800

    mongo init

---
+++ example03-mongo/src/main/java/com/zhang/example03mongo/Example03MongoApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.example03mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example03MongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example03MongoApplication.class, args);
+    }
+
+}
---
+++ example03-mongo/src/test/java/com/zhang/example03mongo/Example03MongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhang.example03mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example03MongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
