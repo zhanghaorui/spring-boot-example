d7f216df827b3d36be9851a9bcea18e1d40311e3
张浩锐 <Zhr199707>
2021-03-05 10:30:53 +0800

    mq init

---
+++ example-04-rabbitmq/src/main/java/com/zhang/example04rabbitmq/Example04RabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.example04rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example04RabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example04RabbitmqApplication.class, args);
+    }
+
+}
---
+++ example-04-rabbitmq/src/test/java/com/zhang/example04rabbitmq/Example04RabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhang.example04rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example04RabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
